f4ac5da551f41dad3498602b9fa79f8f964b290f
Andrew <sherlockh989@gmail.com>
2022-09-11 13:33:44 +0300

    Поправили код первого урока

---
+++ lesson1-servlet-api/src/main/java/BootstrapListener.java
@@ -0,0 +1,19 @@
+import com.sherlock.persist.User;
+import com.sherlock.persist.UserRepository;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class BootstrapListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        UserRepository userRepository = new UserRepository();
+        userRepository.insert(new User("Vladimir"));
+        userRepository.insert(new User("Anastasia"));
+        userRepository.insert(new User("Evgeniy"));
+        sce.getServletContext().setAttribute("userRepository", userRepository);
+    }
+}
